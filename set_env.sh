fb098a41d11bc07ac59391d2f7329a4a541d0841
Chukwuemeka Ashimole <eashimole@yahoo.com>
2021-10-02 22:47:06 +0100

    udagram commit

---
+++ set_env.sh
@@ -0,0 +1,12 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=udagram_admin
+export POSTGRES_PASSWORD=udagram-postgres-db
+export POSTGRES_HOST=udagram-db.cv7bjicptshn.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=udagram-555828831143-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=hello-worlddd!
+export URL=http://localhost:8100
+export PORT=8080
